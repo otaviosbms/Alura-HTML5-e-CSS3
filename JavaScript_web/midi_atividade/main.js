4d0a6169875b82f994ba67a1d66c2be23de743fa
Otávio sbms <105327547+otaviosbms@users.noreply.github.com>
2022-12-14 03:47:25 -0300

    Add files via upload

--- JavaScript_web/midi_atividade/main.js
+++ JavaScript_web/midi_atividade/main.js
@@ -1,9 +1,30 @@
 
 
-function TocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function TocaSom (idelementoaudio) {
+    document.querySelector(idelementoaudio).play();
 }
 
 const listadeteclas = document.querySelectorAll('.tecla');
 
-listadeteclas[0].onclick = TocaSomPom;
+
+let contador = 0;
+
+
+while (contador < listadeteclas.length) {
+
+//verção com for: (desabilitar o primeiro e o ultimo contador)
+//for (let contador = 0; contador < listadeteclas.length; contador++) {
+
+
+    const tecla = listadeteclas[contador];
+    const instrumento = tecla.classList[1];
+    const idaudio = `#som_${instrumento}`; //template string
+
+
+    tecla.onclick = function () {
+        TocaSom(idaudio)
+    }
+
+    contador=contador + 1; // <-- este comendo pode ser torcado por "contador++;"
+
+}
