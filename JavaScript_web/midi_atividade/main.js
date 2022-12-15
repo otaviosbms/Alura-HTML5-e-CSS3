1e9ef87d55d3ac3cacf9a51a7cd0f2f62bd5af2f
Otávio sbms <105327547+otaviosbms@users.noreply.github.com>
2022-12-15 03:40:00 -0300

    Add files via upload

--- JavaScript_web/midi_atividade/main.js
+++ JavaScript_web/midi_atividade/main.js
@@ -1,19 +1,29 @@
 
 
-function TocaSom (idelementoaudio) {
-    document.querySelector(idelementoaudio).play();
+function TocaSom (seletoraudio) {
+    const elemento = document.querySelector(seletoraudio);
+
+    //\/ segurança do codigo no console \/ (não fazer o uso inapropriado da função TocaSom)
+
+    if(elemento /* != null */ && elemento.localName === 'audio'){   // && = and (no JS, não é necessario escrever que o elemento é diferente de null, pois o codigo já sabe que ele não existe)
+        elemento.play();
+    }
+   else(){
+    console.log('Elemento não encontrado ou seletor inválido')
+   }
 }
+
+
 
 const listadeteclas = document.querySelectorAll('.tecla');
 
 
-let contador = 0;
+//let contador = 0;
 
 
-while (contador < listadeteclas.length) {
-
-//verção com for: (desabilitar o primeiro e o ultimo contador)
-//for (let contador = 0; contador < listadeteclas.length; contador++) {
+//while (contador < listadeteclas.length) {
+//verção com while: (ligar o primeiro e o ultimo contador)
+for (let contador = 0; contador < listadeteclas.length; contador++) {
 
 
     const tecla = listadeteclas[contador];
@@ -24,7 +34,22 @@ while (contador < listadeteclas.length) {
     tecla.onclick = function () {
         TocaSom(idaudio)
     }
+    
+    tecla.onkeydown=function(evento){
 
-    contador=contador + 1; // <-- este comendo pode ser torcado por "contador++;"
+        if (evento.code ==='Space' || evento.code ==='Enter'){ // || = or
+            tecla.classList.add('ativa');
+        }
 
+    }
+
+    tecla.onkeyup=function(){
+        tecla.classList.remove('ativa');
+    }
+
+
+
+    //contador=contador + 1; // <-- este comendo pode ser torcado por "contador++;"
 }
+
+
