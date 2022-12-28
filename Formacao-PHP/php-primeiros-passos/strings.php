d18cca8cf83068129e3254c198229ce7e7013daa
Otávio sbms <105327547+otaviosbms@users.noreply.github.com>
2022-12-27 21:54:04 -0300

    Add files via upload

---
+++ Formacao-PHP/php-primeiros-passos/strings.php
@@ -0,0 +1,8 @@
+<?php 
+
+$idade = 18;
+
+
+// pode ser utilizado ' ou " para criar strings, porem as " reconhecem a formatação do texto automaticamente, já as ' APENAS TRANSFORMA TUDO EM STRING.
+
+echo "Olá mundo.\n eu tenho $idade anos!"; // CARACTERES ESPECIAIS (DE ESCAPE)| \n = quebra de linhas || \t = tab 
