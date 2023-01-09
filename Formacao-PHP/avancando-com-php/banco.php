3b68c6a6bc189b674be6c315f4e28569cfc32a60
Otávio sbms <105327547+otaviosbms@users.noreply.github.com>
2023-01-09 00:22:00 -0300

    Add files via upload

--- Formacao-PHP/avancando-com-php/banco.php
+++ Formacao-PHP/avancando-com-php/banco.php
@@ -1,5 +1,37 @@
 <?php
 
+function exibemensagem(string $mensagem){
+    echo $mensagem .PHP_EOL;
+}
+
+
+function sacar(array $conta, float $valorasacar): array
+{
+    if ($valorasacar > $conta['saldo']){
+        exibemensagem('Voce não pode sacar');
+    }else{
+        $conta['saldo'] -= $valorasacar;
+    }
+    return $conta;
+}
+
+function depositar(array $conta, float $valoradepositar): array
+{
+    if ($valoradepositar > 0){
+        $conta['saldo'] += $valoradepositar;
+    }else{
+        exibemensagem('depositos precisam ser positivos');
+    }
+    
+    return $conta;
+}
+
+
+
+// utilizar o return no codigo de uma função faz com que a funcionalidade dela coniga ser armazenada em uma variavel. exp:   $var = funct();
+
+
+
 
 
 $contascorrentes = [
@@ -21,8 +53,21 @@ $contascorrentes = [
 
 ];
 
-$contascorrentes['123.456.789-11']['saldo'] -= 500;
+
+
+
+
+
+
+$contascorrentes['123.456.789-12'] = sacar($contascorrentes['123.456.789-12'], 500);
+
+$contascorrentes['123.456.789-10'] = depositar($contascorrentes['123.456.789-10'], 500);
+
+
+
+
+
 
 foreach ($contascorrentes as $cpf => $conta){
-    echo $cpf." ".$conta['titular']." ".$conta['saldo'].PHP_EOL;
+    exibemensagem($cpf." ".$conta['titular']." ".$conta['saldo']);
 }
