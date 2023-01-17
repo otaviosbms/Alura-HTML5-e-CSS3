9b6aa844cefc98a6312bd1b445ae60fb21e79ae7
Otávio sbms <105327547+otaviosbms@users.noreply.github.com>
2023-01-17 02:26:16 -0300

    Add files via upload

--- Formacao-PHP/manipulando-colecoes-com-php/bimestres.php
+++ Formacao-PHP/manipulando-colecoes-com-php/bimestres.php
@@ -11,10 +11,24 @@ $notasbimestre1 = [
 $notasbimestre2 = [
     'ana' => 9,
     'joao' => 8,
-    'maria' => 10,
     'roberto' => 7,
 ];
 
-var_dump(array_diff_key($notasbimestre1, $notasbimestre2)); // array_diff($array1, $array2, ...) -> faz um array (COM BASE NOS VALORES) mostrando a diferença entre o primeiro array e os outros.
-                                                            // array_diff_key($array1, $array2, ...) -> faz um array (COM BASE NAS CHAVES) mostrando a diferença entre o primeiro array e os outros.
-                                                            // array_diff_assoc($array1, $array2, ...) -> faz um array (COM BASE EM AMBOS) mostrando a diferença entre o primeiro array e os outros.
+
+// array_diff($array1, $array2, ...) -> faz um array (COM BASE NOS VALORES) mostrando a diferença entre o primeiro array e os outros.
+// array_diff_key($array1, $array2, ...) -> faz um array (COM BASE NAS CHAVES) mostrando a diferença entre o primeiro array e os outros.
+// array_diff_assoc($array1, $array2, ...) -> faz um array (COM BASE EM AMBOS) mostrando a diferença entre o primeiro array e os outros.
+
+
+$alunosfaltantes = array_diff_key($notasbimestre1, $notasbimestre2);
+
+$nomesalunos = array_keys($alunosfaltantes);
+$notasalunos = array_values($alunosfaltantes);
+
+// array_combine($array1(chave), $array2(valor)) -> combina arrays com o mesmo tamanho.
+
+var_dump(array_combine($notasalunos, $nomesalunos));
+
+// array_flip($array) -> inverte o array (entre chaves e valores).
+
+var_dump(array_flip($alunosfaltantes));
