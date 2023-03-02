06c47de91ab0258b68d2ec3ce83562301f1999a7
Otávio sbms <105327547+otaviosbms@users.noreply.github.com>
2023-03-02 16:23:28 -0300

    Add files via upload

---
+++ Formacao-PHP/PHP-Strings/explode.php
@@ -0,0 +1,6 @@
+<?php
+
+$array = '1 2 3 4 ';
+
+// \/ O terceiro parametro do explode delimita a auqntidade de chaves que o meu arrar com as informações será gerado \/
+var_dump(explode(' ', $array, 2 ));
---
+++ Formacao-PHP/PHP-Strings/telefones.php
@@ -0,0 +1,6 @@
+<?php
+
+$telefones = ['(24) 9999 - 9999', '(21) 9999 - 9999', '(24) 9999 - 9999'];
+
+// \/ transforma um array em um conjunto de strings separadas por uma quebra de linha \/
+echo implode(PHP_EOL,$telefones).PHP_EOL;
