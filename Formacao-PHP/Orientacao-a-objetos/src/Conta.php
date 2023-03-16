abdf80faae8ee7144b1fc3200d5f9c0b0dd94d61
Otávio sbms <105327547+otaviosbms@users.noreply.github.com>
2023-03-15 22:15:56 -0300

    Add files via upload

---
+++ Formacao-PHP/Orientacao-a-objetos/src/Conta.php
@@ -0,0 +1,15 @@
+<?php
+
+// \ SOBRE CLASSES E OBJETOS: /
+// Uma class pode ser comparada a uma forma de bolo, onde por meio dele são produzidos os objetos
+// que seriam os bolos. 
+//para facil entendimento deve ser pronunciado: "objeto" do TIPO "nome da classe".
+// a variavel é APENAS um endereço para onde o objeto é criado.
+
+class Conta
+{
+    public $cpfTitular;
+    public $nomeTitular;
+    public $saldo;
+    
+}
