0186a4f29f47ff7bfa889245eeaedb680d1efee5
Otávio sbms <105327547+otaviosbms@users.noreply.github.com>
2023-03-24 02:05:03 -0300

    Add files via upload

--- Formacao-PHP/Orientacao-a-objetos/banco.php
+++ Formacao-PHP/Orientacao-a-objetos/banco.php
@@ -3,10 +3,14 @@
 require_once 'src/Conta.php';
 
 $primeiraConta = new Conta('123.456.789-10', 'Vinicius Dias');
-
 $primeiraConta->depositar(500);
 $primeiraConta->sacar(300);
 
 echo $primeiraConta->recuperarNomeTitular().PHP_EOL;
 echo $primeiraConta->recuperarCpfTitular().PHP_EOL;
 echo $primeiraConta->recuperarSaldo().PHP_EOL;
+
+$segundaConta = new Conta('098.765.432-11', 'Patricia');
+var_dump($segundaConta);
+
+echo Conta::recuperarNumeroDeContas();
